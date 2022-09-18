2cdd03b180ec4074f126f641d0eb693b8896a66d
yejine2 <yejine2@naver.com>
2022-09-18 22:31:35 +0900

    conf: Add react icons & Remove router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './services/store'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -10,8 +9,6 @@ const root = createRoot(container)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>,
 )
