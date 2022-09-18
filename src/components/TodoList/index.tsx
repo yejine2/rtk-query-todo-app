39574deeb9fa21705cb9008f7393786a146266f0
yejine2 <yejine2@naver.com>
2022-09-18 22:44:55 +0900

    feat: Add todo data

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import * as S from './style'
+import TodoItem from '../TodoItem'
+import { useTodosQuery } from '../../services/todoApi'
 
 function TodoList() {
-  return <div>TodoList</div>
+  const { data: todos, isLoading, isFetching } = useTodosQuery()
+
+  if (isLoading || isFetching) {
+    return <S.Loader>Lodaing...</S.Loader>
+  }
+
+  return (
+    <S.TodoListBlock>
+      {todos?.map((todo: any) => (
+        <div key={todo.id}>
+          <TodoItem todo={todo} />
+        </div>
+      ))}
+    </S.TodoListBlock>
+  )
 }
 
 export default TodoList
