c98cb6dd295589e31ba52fd007e2c10b7bab1868
yejine2 <yejine2@naver.com>
2022-09-18 22:46:39 +0900

    design: Add TodoItem style

--- src/components/TodoItem/style.ts
+++ src/components/TodoItem/style.ts
@@ -1,1 +1,60 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+type TodoItemType = {
+  done: boolean
+}
+
+export const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 2rem;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`
+
+export const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`
+
+export const CheckCircle = styled.div<TodoItemType>`
+  width: 3.2rem;
+  height: 3.2rem;
+  border-radius: 1.6rem;
+  border: 1px solid #ced4da;
+  font-size: 2.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 2rem;
+  cursor: pointer;
+  ${({ done }) =>
+    done &&
+    css`
+      border: 1px solid #ff9800;
+      color: #ff9800;
+    `}
+`
+
+export const Text = styled.div<TodoItemType>`
+  flex: 1;
+  font-size: 1.9rem;
+  color: #495057;
+  ${({ done }) =>
+    done &&
+    css`
+      color: #ced4da;
+    `}
+`
