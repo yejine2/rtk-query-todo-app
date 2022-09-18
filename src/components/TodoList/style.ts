07b55465ff30f5d68e5f58fb13c222564a1ab117
yejine2 <yejine2@naver.com>
2022-09-18 22:45:32 +0900

    design: Add TodoList style

--- src/components/TodoList/style.ts
+++ src/components/TodoList/style.ts
@@ -1,1 +1,13 @@
 import styled from 'styled-components'
+
+export const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 2rem 3.2rem;
+  padding-bottom: 4.8rem;
+  overflow-y: auto;
+`
+export const Loader = styled.div`
+  margin: 2rem auto;
+  font-size: 1.8rem;
+  color: #ff9800;
+`
