3318dc75b081761fffc9f881697b9f2c583d1086
yejine2 <yejine2@naver.com>
2022-09-19 11:51:54 +0900

    feat: Add Todo Add button

--- src/components/TodoAdd/index.tsx
+++ src/components/TodoAdd/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as S from './style'
+import { MdAdd } from 'react-icons/md'
 
 function TodoAdd() {
-  return <div>TodoAdd</div>
+  const [open, setOpen] = useState(false)
+
+  const onToggle = () => setOpen(!open)
+
+  return (
+    <>
+      {open && (
+        <S.InsertFormPositioner>
+          <S.InsertForm>
+            <S.Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+            />
+          </S.InsertForm>
+        </S.InsertFormPositioner>
+      )}
+      <S.CircleButton onClick={onToggle} open={open}>
+        <MdAdd />
+      </S.CircleButton>
+    </>
+  )
 }
 
 export default TodoAdd
