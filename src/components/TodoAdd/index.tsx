021b7ff2534ea4ea29cef5d5a76fb2c74495e278
yejine2 <yejine2@naver.com>
2022-09-20 23:46:00 +0900

    feat: Modify TodoAdd component

--- src/components/TodoAdd/index.tsx
+++ src/components/TodoAdd/index.tsx
@@ -1,20 +1,36 @@
 import React, { useState } from 'react'
 import * as S from './style'
 import { MdAdd } from 'react-icons/md'
+import { useAddTodoMutation } from '../../services/todoApi'
 
 function TodoAdd() {
   const [open, setOpen] = useState(false)
+  const [value, setValue] = useState('')
+  const [addTodo] = useAddTodoMutation()
 
   const onToggle = () => setOpen(!open)
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value)
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault()
+    await addTodo({
+      id: Math.random(),
+      title: value,
+      done: false,
+    })
+  }
 
   return (
     <>
       {open && (
         <S.InsertFormPositioner>
-          <S.InsertForm>
+          <S.InsertForm onSubmit={onSubmit}>
             <S.Input
               autoFocus
               placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
             />
           </S.InsertForm>
         </S.InsertFormPositioner>
