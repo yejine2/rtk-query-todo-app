76b76b8b2a65b300523d5d9d0e07b09f1112414d
yejine2 <yejine2@naver.com>
2022-09-21 23:33:30 +0900

    feat: Modify undoneTasks message

--- src/components/TodoHead/index.tsx
+++ src/components/TodoHead/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import * as S from './style'
+import { useTodosQuery } from '../../services/todoApi'
 
 function TodoHead() {
+  const { data: todos } = useTodosQuery()
   const today = new Date()
 
   const dateString = today.toLocaleString('ko-KR', {
@@ -12,11 +14,13 @@ function TodoHead() {
 
   const dayName = today.toLocaleString('ko-KR', { weekday: 'long' })
 
+  const undoneTasks = todos?.filter((todo) => !todo.done)
+
   return (
     <S.TodoHeadBlock>
       <h1>{dateString}</h1>
       <div className="day">{dayName}</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <div className="tasks-left">할 일 {undoneTasks?.length}개 남음</div>
     </S.TodoHeadBlock>
   )
 }
