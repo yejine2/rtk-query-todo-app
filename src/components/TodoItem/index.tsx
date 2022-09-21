0688797a8bd2e775b8e65c79cfad2c69765c477c
yejine2 <yejine2@naver.com>
2022-09-21 23:26:03 +0900

    feat: Add onDeleteTodo function in remove button

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import * as S from './style'
 import { MdDone, MdDelete } from 'react-icons/md'
 import { Todo } from '../../models/todo.model'
+import { useDeleteTodoMutation } from '../../services/todoApi'
 
 type TodoProps = {
   todo: Todo
 }
 
 function TodoItem({ todo }: TodoProps) {
+  const [deleteTodo] = useDeleteTodoMutation()
+
+  const onDeleteTodo = (id: any) => {
+    deleteTodo(id)
+  }
+
   return (
     <S.TodoItemBlock>
       <S.CheckCircle done={todo.done}>{todo.done && <MdDone />}</S.CheckCircle>
       <S.Text done={todo.done}>{todo.title}</S.Text>
-      <S.Remove>
+      <S.Remove onClick={() => onDeleteTodo(todo.id)}>
         <MdDelete />
       </S.Remove>
     </S.TodoItemBlock>
